63cc60aff45e0688a3613c9cbf9f3efb4ab95610
Timofeev Yury <yurik-007@mail.ru>
2024-07-28 01:14:30 +0300

    make burger menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(function () {
-  var mixer = mixitup('.gallery__content')
   Fancybox.bind("[data-fancybox]", {})
   $('.blog-slider__items').slick({
     dots: true,
     arrows:false
   });
+
+  $('.menu__btn').on('click', function (e) {
+    $('.header__top-inner').toggleClass('header__top-inner--active')
+  })
+
+  var mixer = mixitup('.gallery__content')
 
 })
